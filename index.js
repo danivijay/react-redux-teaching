37bd9f4f93cd8abbc1fecc4187109a5e854cac4b
Dani Vijay <danimvijay@gmail.com>
2018-05-18 22:16:01 +0530

    add basic pgm

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 import { createStore } from 'redux'
 
 const reducer = (state, action) => {
-  // TODO
-}
+  switch (action.type) {
+    case "ADD":
+      state = state + action.payload;
+      break;
+    case "SUBTRACT":
+      state = state - action.payload;
+      break;
+  }
+  return state;   
+};
 
-const store = createStore(, 1)
+const store = createStore(reducer, 1);
+
+store.subscribe(() => {
+  console.log("store updated!", store.getState());
+})
+
+store.dispatch({
+  type: "ADD",
+  payload: 10
+});
